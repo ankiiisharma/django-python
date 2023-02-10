d974f7af0c7d7c6960cbb42d6e7827e948b237e8
ankiiisharma <myselfankit.2003@gmail.com>
2023-02-10 20:45:21 +0530

    Django

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+   return render(request, 'index.html')
+   # return HttpResponse('HOMEPAGE')
+
+def about(request):
+   return render(request, 'about.html') 
+
+def services(request):
+    return render(request, 'services.html')  
+
+def contact(request):
+    return render(request, 'contact.html')
+    
